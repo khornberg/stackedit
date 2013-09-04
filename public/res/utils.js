9f9d17232f9ee5c74ad825e1424e357d8de8fbfe
OpenShift guest <kyle.hornberg@gmail.com>
2013-09-04 15:45:12 -0400

    Added Save As PDF. Depends on jsPDF. Only one page is saved. This is jsPDF issue #81.

--- public/res/utils.js
+++ public/res/utils.js
@@ -273,44 +273,37 @@ define([
             
             // source can be HTML-formatted string, or a reference
             // to an actual DOM element from which the text will be scraped.
-            ,
-                source = content //$('#preview-contents')[0]
+            , source = content //$('#preview-contents')[0]
             
-                // we support special element handlers. Register them with jQuery-style
-               // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
-                // There is no support for any other type of selectors
-                // (class, of compound) at this time.
-                ,
-                specialElementHandlers = {
-                    // element with id of "bypass" - jQuery style selector
-                    '#bypassme': function(element, renderer) {
-                        // true = "handled elsewhere, bypass text extraction"
-                        return true
-                    }
+            // we support special element handlers. Register them with jQuery-style
+            // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+            // There is no support for any other type of selectors
+            // (class, of compound) at this time.
+            , specialElementHandlers = {
+                // element with id of "bypass" - jQuery style selector
+                '#bypassme': function(element, renderer) {
+                    // true = "handled elsewhere, bypass text extraction"
+                    return true
                 }
+            }
             
-                // all coords and widths are in jsPDF instance's declared units
-                // 'inches' in this case
-                pdf.fromHTML(
-                source // HTML string or DOM elem ref.
-                ,
-                0.5 // x coord
-                ,
-                0.5 // y coord
-                ,
-                {
-                    'width': 7.5 // max width of content on PDF
-                    ,
-                    'elementHandlers': specialElementHandlers
-                })
+            // all coords and widths are in jsPDF instance's declared units
+            // 'inches' in this case
+            pdf.fromHTML(
+            source // HTML string or DOM elem ref.
+            , 0.5 // x coord
+            , 0.5 // y coord
+            ,
+            {
+                'width': 7.5 // max width of content on PDF
+                , 'elementHandlers': specialElementHandlers
+            })
             
             pdf.save(filename);
         }
         catch (e) {
             //TODO Add error message saying PDF could not be created
-            //Tried eventMgr.onError(e); but I've given a not defined error
-            //Tried adding to the top but given another error in the console
-            console.log('no pdf for you :|');
+            console.log('no pdf for you :|' + e);
         }
     };
 
